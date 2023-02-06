118d87ccf1d836c1bce0dab101ecbd8df3199f21
unknown <you@example.com>
2023-02-06 22:33:55 +0300

    feat: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const positiveChar = Math.abs(n);
+    const char = positiveChar.toString();
+    let charReverse = "";
+    for (let i = 0; i < char.length; i = i + 1) {
+        if (char[char.charlength - 1] !== '0') {
+            charReverse = char[i] + charReverse;
+        } else {
+            charReverse = charReverse;
+        }
+        
+    };
+    return charReverse;
+};
